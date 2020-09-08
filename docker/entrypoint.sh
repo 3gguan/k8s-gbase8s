434c933fdccf092607ab26c149991523b1ffc708
3gguan <3gguan@gmail.com>
2020-09-08 17:51:57 +0800

    增加gbasedbt用户密码

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /opt/gbase8s/env.sh
+source /env.sh
 
 if [ -z $GBASEDBTDIR ]; then
   echo "GBASEDBTDIR not exists"
@@ -25,33 +25,61 @@ init_dbspaces() {
 
 DBSPACES="rootdbs plogdbs llogdbs tmpdbs01 tmpdbs02 datadbs01 datadbs02 datadbs03 datadbs04 datadbs05 datadbs06 datadbs07 datadbs08"
 
-chown gbasedbt:gbasedbt $GBASEDBTDIR/etc/onconfig.ol_generaldata1210 $GBASEDBTDIR/etc/sqlhosts.ol_generaldata1210 $GBASEDBTDIR/logs $GBASEDBTDIR/storage
+change_permissions() {
+  chown gbasedbt:gbasedbt $GBASEDBTDIR/logs $GBASEDBTDIR/storage
+}
 
-if [ -f $GBASEDBTDIR/storage/rootdbs ]; then
-  oninit -vwy
-else
-  create_dbspaces
-  oninit -iwvy
-  init_dbspaces
-  onmode -ky
-  onclean -ky
-  oninit -vwy
-fi
+set_gbasedbt_password() {
+  temp_password=${GBASEDBT_PASSWORD:-"gbasedbt"} 
+  echo -e "$temp_password\n$temp_password" | passwd gbasedbt
+}
+
+check_health() {
+  if [ `ps -ef | grep oninit | grep -v grep | wc -l` -gt 2 ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+main() {
+  set_gbasedbt_password
+  change_permissions
+
+  if [ -f $GBASEDBTDIR/storage/rootdbs ]; then
+    oninit -vwy
+  else
+    create_dbspaces
+    oninit -iwvy
+    init_dbspaces
+    onmode -ky
+    onclean -ky
+    oninit -vwy
+  fi
+
+  while true
+  do
+    sleep 10 & wait
+    check_health
+    if [ $? == 0 ]; then
+      echo "oninit exit"
+      exit 0
+    fi
+  done
+}
 
 close_all()
 {
   onmode -ky
   onclean -ky
-  for i in `ps -ef|grep tail|grep -v grep|awk '{print $2}'`
+  for i in `ps -ef|grep sleep|grep -v grep|awk '{print $2}'`
   do
     kill $i
   done
   exit 0
 }
+
 trap "echo \"RECV SIGINT\"; close_all" SIGINT
 trap "echo \"RECV SIGTERM\"; close_all" SIGTERM
 
-while true
-do
-  tail -f /dev/null & wait
-done
+main
