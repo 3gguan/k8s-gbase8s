f7311ff3eb5a9df26f7bda5809657ed315bf18e6
3gguan <3gguan@gmail.com>
2020-09-30 14:12:51 +0800

    新增注释

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#引入环境变量
 import_env() {
   source /env.sh
 
@@ -9,6 +10,7 @@ import_env() {
   fi
 }
 
+#初始化数据库实例时创建dbspace
 create_dbspaces() {
   for i in $DBSPACES;
   do
@@ -16,6 +18,7 @@ create_dbspaces() {
   done
 }
 
+#初始化dbspace
 init_dbspaces() {
   for i in $DBSPACES;
   do
@@ -25,17 +28,21 @@ init_dbspaces() {
   done
 }
 
+#初始化时需要创建的dbspace
 DBSPACES="rootdbs plogdbs llogdbs tmpdbs01 tmpdbs02 datadbs01 datadbs02 datadbs03 datadbs04 datadbs05 datadbs06 datadbs07 datadbs08"
 
+#修改用户及用户组
 change_permissions() {
   chown gbasedbt:gbasedbt $GBASEDBTDIR/logs $GBASEDBTDIR/storage $GBASEDBTDIR/etc/onconfig.ol_gbasedbt_1 $GBASEDBTDIR/etc/sqlhosts.ol_gbasedbt_1
 }
 
+#设置gbasedbt的密码，由GBASEDBT_PASSWORD指定，默认为gbasedbt
 set_gbasedbt_password() {
   temp_password=${GBASEDBT_PASSWORD:-"gbasedbt"} 
   echo -e "$temp_password\n$temp_password" | passwd gbasedbt
 }
 
+#检查oninit是否还存在
 check_health() {
   if [ `ps -ef | grep oninit | grep -v grep | wc -l` -gt 2 ]; then
     return 1
@@ -44,6 +51,10 @@ check_health() {
   fi
 }
 
+#准备配置文件，用于外部挂载配置文件到容器内。
+#如果ONCONFIG_FILE_NAME SQLHOSTS_FILE_NAME指定了配置文件的位置，
+#就拷贝指定的配置文件到$GBASEDBTDIR/etc/下，并修改env.sh中配置文件的路径和名称
+#从onconfig文件中读取数据库实例名，修改env.sh中的对应变量
 prepare_config_file() {
   if [ -n "$ONCONFIG_FILE_NAME" ]; then
     if [ -f $ONCONFIG_FILE_NAME ]; then
@@ -69,13 +80,26 @@ prepare_config_file() {
   fi
 }
 
+#main函数
 main() {
+
+  #设置gbasedbt密码
   set_gbasedbt_password
+
+  #引入环境变量
   import_env
+
+  #准备配置文件
   prepare_config_file
+  
+  #重新引入环境变量
   import_env
+
+  #修改用户及用户组
   change_permissions
 
+  #如果rootdbs已经存在，表示数据库实例已经初始化过，直接启动oninit
+  #如果不存在，就需要初始化数据库实例
   if [ -f $GBASEDBTDIR/storage/rootdbs ]; then
     oninit -vwy
   else
@@ -87,6 +111,7 @@ main() {
     oninit -vwy
   fi
 
+  #定期检查oninit是否存在，如果不存在，脚本退出，整个容器退出
   while true
   do
     sleep 10 & wait
@@ -98,6 +123,7 @@ main() {
   done
 }
 
+#关闭oninit，并退出脚本的执行
 close_all()
 {
   onmode -ky
@@ -109,6 +135,7 @@ close_all()
   exit 0
 }
 
+#监控两个信号，如果收到SIGINT或SIGTERM，正常关闭oninit，并退出
 trap "echo \"RECV SIGINT\"; close_all" SIGINT
 trap "echo \"RECV SIGTERM\"; close_all" SIGTERM
 
